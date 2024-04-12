6d12581a92f9e5c2834f558082263fc5a3d27fc8
buonuser <>
2024-04-12 15:19:02 +0200

    deploy

---
+++ assets/GiornoView-BLAqDBc-.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Weather.vue_vue_type_script_setup_true_lang-wM1i0fYF.js";import{d as e,e as a,o as t}from"./index-C-R-OpIt.js";import"./index-BnA9jiDo.js";const m=e({__name:"GiornoView",setup(n){return(r,_)=>(t(),a(o,{location:"Milan, Italy"}))}});export{m as default};
